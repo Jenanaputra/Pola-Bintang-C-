82308ae398ae4a6c1b052f57b5cd35185e3b0c46
Jenana Putra <jenanaputra57@yahoo.com>
2022-01-10 16:34:02 +0700

    menambahkan pola bintang No. 4-7

--- pola_pola_bintang.cpp
+++ pola_pola_bintang.cpp
@@ -12,6 +12,10 @@ int main()
     std::cout<<"1. Segitiga bawah dan sisanya bintang"<<" \n";
     std::cout<<"2. Diagonal silang"<<" \n";
     std::cout<<"3. Kotak dengan bagian dalam kosong"<<" \n";
+    std::cout<<"4. Segitiga siku siku bawah"<<" \n";
+    std::cout<<"5. Segitiga siku siku atas"<<" \n";
+    std::cout<<"6. Segitiga sama sisi arah atas"<<" \n";
+    std::cout<<"7. Segitiga sama sisi arah bawah"<<" \n";
     std::cout<<"++++++++++++++++++++++++++++++++++++++"<<" \n";
     std::cout<<"Tentukan pilihanmu lur :";
     std::cin>>switch_var;
@@ -77,6 +81,61 @@ int main()
                 std::cout<<" \n";
             }
         
+            break;
+        case 4 :
+            std::cout<<"Jumlah elemen yang diinginkan :";
+            std::cin>>x;
+            for (int i=0; i<x + 1; i++){
+                for(int j=0; j<i; j++){
+                    std::cout<<"* ";
+                }
+                std::cout<<" \n";
+            }
+
+            break;
+        case 5 :
+            std::cout<<"Jumlah elemen yang diinginkan :";
+            std::cin>>x;
+            for (int i=0; i<x + 1; i++){
+                for(int j=0; j< x + 1; j++){
+                    if (j > i){
+                        std::cout<<"* ";
+                    }else{
+                        std::cout<<"  ";
+                    }
+                }
+                std::cout<<" \n";
+            }
+            break;
+        case 6 :
+            std::cout<<"Jumlah elemen yang diinginkan :";
+            std::cin>>x;
+            for (int i=0; i<x + 1; i++){
+                for(int j=0; j< x + 1; j++){
+                    if (j != (x+1) - i){
+                        std::cout<<" ";
+                    }else{
+                        for(int k=0; k<i; k++){
+                            std::cout<<"* ";                        
+                        }
+                    }
+                }
+                std::cout<<" \n";
+            }
+            break;
+        case 7 :
+            std::cout<<"Jumlah elemen yang diinginkan :";
+            std::cin>>x;
+            for (int i=0; i<x + 1; i++){
+                for(int j=0; j< x + 1; j++){
+                    if (j > i){
+                        std::cout<<"* ";
+                    }else{
+                        std::cout<<" ";
+                    }
+                }
+                std::cout<<" \n";
+            }
             break;
     }
 
